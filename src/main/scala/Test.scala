1e42ed35cace01346bb27d76533e05c173515553
fearlesspandas <landonrenzullo@gmail.com>
2020-01-20 14:26:58 -0500

    join attempt

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -7,22 +7,29 @@ object Test {
   import spark.implicits._
 
   val rand = scala.util.Random
-
+  //startdf should come into the picture after joins on data have been performed
   implicit val startdf = Seq((1,2),(2,3),(3,4)).toDF("ONE","TWO")
 
   class ONE extends AXIOM[ONE]
   class TWO extends AXIOM[TWO]
 
   implicit val three: COL[ONE with TWO ] => COL[THREE] = (ot:COL[ONE with TWO]) => {
-    val one = ot.get[ONE]
-    val two = ot.get[TWO]
-    class T extends COL[THREE](when(one.col.mod(2) === 0,two.col).otherwise(one.col))
+    val one = ot.getcol[ONE]
+    val two = ot.getcol[TWO]
+    class T extends COL[THREE](when(one.mod(2) === 0,two).otherwise(one))
     new T
   }
   implicit val four: COL[THREE] => COL[FOUR] = (three:COL[THREE]) => {
     class T extends COL[FOUR]( when(three.col === "3", lit("is three")).otherwise("not three"))
     new T
   }
+  //WANT THREE TO extend ONE with TWO in some context
+
   class THREE extends Dependency[ONE with TWO,THREE]
+
   class FOUR extends Dependency[THREE,FOUR]
+  //class FIVE extends JOIN[THREE,THREE,ONE]
+
+
+  //three -- dependency[one,three] --- dependency[one with two,three] ---
 }
--- src/main/scala/Typeable.scala
+++ src/main/scala/Typeable.scala
@@ -1,6 +1,7 @@
 package Typical.core;
 
 
+
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
 
@@ -9,10 +10,12 @@ import scala.reflect.runtime.{universe => ru}
 import ru._
 
 
-object Typeable{
-  abstract class COL[ +A <: COL[_] ](coldef:Column)(implicit taga:ClassTag[A]) extends Column(classTag[A].runtimeClass.getSimpleName()){
+object Typeable {
+
+  abstract class COL[+A <: COL[_]](coldef: Column)(implicit taga: ClassTag[A]) extends Column(classTag[A].runtimeClass.getSimpleName()) {
     val col = this.coldef
-    def get[U>:A<:AXIOM[U]](implicit tag: TypeTag[U]):U = {
+
+    def get[U >: A <: AXIOM[U]](implicit tag: TypeTag[U]): U = {
       val m = ru.runtimeMirror(getClass().getClassLoader())
       val classu = ru.typeOf[U].typeSymbol.asClass
       val cm = m.reflectClass(classu)
@@ -20,39 +23,56 @@ object Typeable{
       val ctorm = cm.reflectConstructor(ctor)
       ctorm().asInstanceOf[U]
     }
+    def getcol[U >: A <: AXIOM[U]](implicit tag: TypeTag[U]): Column = get[U].col
   }
-  abstract class Dependency[-A<:COL[_],+B<:Dependency[_,B] with COL[B]](implicit ev: COL[A] => COL[B],taga:ClassTag[A],tagb: ClassTag[B]) extends COL[B](coldef = ev(classTag[A].runtimeClass.newInstance().asInstanceOf[COL[A]]).col){
+
+  //trait E[+A<:E[_]]
+  abstract class Dependency[-A <: COL[_], +B <: Dependency[_, B] with COL[B]](implicit ev: COL[A] => COL[B], taga: ClassTag[A], tagb: ClassTag[B]) extends COL[B](coldef = ev(classTag[A].runtimeClass.newInstance().asInstanceOf[COL[A]]).col)  {
     val f = this.ev
   }
-  implicit class DAT[ +A <: DAT[_] ](datadef:DataFrame)(implicit startdf:DataFrame,taga:ClassTag[A]){
-    val df = if(datadef == null) this.startdf else this.datadef
+
+  implicit class DAT[+A <: DAT[_]](datadef: DataFrame)(implicit startdf: DataFrame, taga: ClassTag[A]) {
+    val df = if (datadef == null) this.startdf else this.datadef
+
+    def toDAT() = this
   }
-  abstract class AXIOM[+A<:AXIOM[A]](implicit initdf:DataFrame,taga:ClassTag[A]) extends COL[A](initdf.col(classTag[A].runtimeClass.getSimpleName()))
+
+  abstract class AXIOM[+A <: AXIOM[A]](implicit initdf: DataFrame, taga: ClassTag[A]) extends COL[A](initdf.col(classTag[A].runtimeClass.getSimpleName()))
+
   //col types can only reference a start df
-  implicit class COLTODAT[+A<:COL[_]](c:COL[A])(implicit tag: ClassTag[COLTODAT[A]],initdf:DataFrame) extends DAT[COLTODAT[A]](
-    initdf.withColumn(c.toString(),c.col)
-  )(startdf=initdf,tag)
-  {
+  implicit class COLTODAT[A <: COL[_]](c: COL[A])(implicit tag: ClassTag[COLTODAT[A]], initdf: DataFrame) extends DAT[COLTODAT[A]](
+    initdf.withColumn(c.toString(), c.col)
+  )(startdf = initdf, tag) {
     val iscol = true;
-    def add[T<:Dependency[A,T]]()(implicit tagt:ClassTag[T]):DataFrame = {
+    val todf = this.df
+
+    def add[T <: Dependency[A, T]]()(implicit tagt: ClassTag[T]): DataFrame = {
       val t = classTag[T].runtimeClass.newInstance().asInstanceOf[T]
-      val u = c//.get[COL[U]]
-      this.df.withColumn(t.toString(),t.f(u).col)
+      val u = c //.get[COL[U]]
+      this.df.withColumn(t.toString(), t.f(u).col)
     }
   }
-  implicit class AXTODAT[+A<:AXIOM[A]](c:AXIOM[A])(implicit tag: ClassTag[A],startdf:DataFrame) extends DAT[AXTODAT[A]](null){
+
+  implicit class AXTODAT[A <: AXIOM[A]](c: AXIOM[A])(implicit tag: ClassTag[A], startdf: DataFrame) extends DAT[AXTODAT[A]](null) {
     val iscol = true;
-    def add[T<:Dependency[A,T]]()(implicit tagt:ClassTag[T]):DataFrame = {
+
+    def add[T <: Dependency[A, T]]()(implicit tagt: ClassTag[T]): DataFrame = {
       val t = classTag[T].runtimeClass.newInstance().asInstanceOf[T]
-      val u = c//.get[COL[U]]
-      this.df.withColumn(t.toString(),t.f(u).col)
+      val u = c //.get[COL[U]]
+      this.df.withColumn(t.toString(), t.f(u).col)
     }
   }
+
   //dependent types only reference dependencies dataframe
-  implicit class DEPTODAT[B<:COL[_],A<:Dependency[B,A]](dep:A)(implicit taga: ClassTag[A],tagb:ClassTag[B],ttagb: TypeTag[B],startdf:DataFrame) extends DAT[DEPTODAT[B,A]](
-    datadef= (classTag[B]).runtimeClass.newInstance().asInstanceOf[COL[B]].add[A]
-  ){
+  implicit class DEPTODAT[B <: COL[_], A <: Dependency[B, A]](dep: A)(implicit taga: ClassTag[A], tagb: ClassTag[B], ttagb: TypeTag[B], startdf: DataFrame) extends DAT[DEPTODAT[B, A]](
+    datadef = (classTag[B]).runtimeClass.newInstance().asInstanceOf[COL[B]].add[A]
+  ) {
     val isDep = true;
     override val df = (classTag[B]).runtimeClass.newInstance().asInstanceOf[COL[B]].add[A]
   }
+
+//  abstract class JOIN[+A <: Dependency[_,A], +B <: Dependency[_,B], C <: AXIOM[C]](implicit cInA: A <:< Dependency[C,A]  , cInB: B <:< Dependency[C,B] , taga: ClassTag[A], tagb: ClassTag[B], tagc: ClassTag[C], startdf: DataFrame) extends DAT[JOIN[A, B, C]](datadef = {
+//    classTag[A].runtimeClass.newInstance().asInstanceOf[COL[A]].todf.join(classTag[B].runtimeClass.newInstance().asInstanceOf[COL[B]].todf, classTag[C].runtimeClass.newInstance().asInstanceOf[COL[C]].toString())
+//  })
+
 }
